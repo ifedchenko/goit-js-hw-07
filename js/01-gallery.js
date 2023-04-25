92342fc8116e049a99e00a7c13ded6a22471bb4f
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-04-25 16:43:57 +0300

    Finish task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryList = document.querySelector('.gallery');
+const galleryList = document.querySelector(".gallery");
 
-const galleryItem = galleryItems.map(({preview, original, description}) => {
-    return `<li class ="gallery__item"><a href="${original}" class="gallery__link " > <img class ="gallery__image" src="${preview}" alt="${description}" width="340"/></a></li>`
-})
-galleryList.insertAdjacentHTML('beforeend', galleryItem.join(''))
+const galleryItem = galleryItems.map(
+  ({ preview, original, description }) => {
+    return `<li class ="gallery__item"><a class="gallery__link" href="${original}"> <img class ="gallery__image" src="${preview}" data-source="${original}" alt="${description}" width="340"/></a></li>`;
+  }
+);
+galleryList.insertAdjacentHTML("beforeend", galleryItem.join(""));
 
+galleryList.addEventListener("click", onClickOpenImg);
 
+function onClickOpenImg(event) {
+  event.preventDefault();
 
+  // console.log(linkSrc);
+  //   console.log(event.target.getAttribute("alt"));
+  const linkSrc = event.target.parentElement.getAttribute("href");
+  const altSrc = event.target.getAttribute("alt");
+
+  const instance = basicLightbox.create(`
+		<img width="1280" src="${linkSrc}" alt="${altSrc}" >
+	`);
+
+  instance.show();
+
+  function escKeyDown(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", escKeyDown);
+    }
+  }
+
+  function removeListeners() {
+    instance.element().removeEventListener("click", removeListeners);
+    window.removeEventListener("keydown", escKeyDown);
+  }
+  //   console.log(instance);
+  //   console.log(instance.element());
+
+  window.addEventListener("keydown", escKeyDown);
+
+  instance.element().addEventListener("click", removeListeners);
+}
